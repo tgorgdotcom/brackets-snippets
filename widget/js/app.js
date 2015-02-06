58cff5bc3b0a5881293861b33a8527dad61057b1
Edward Chu <crazyzyt@gmail.com>
2015-02-06 10:56:11 +0800

    load screen demo locally

--- widget/js/app.js
+++ widget/js/app.js
@@ -1,3 +1,16 @@
-define('app', ['angular'], function(angular) {
-  return angular.module('snippets-manager', []);
+define('app', ['angular', 'require'], function(angular, require) {
+  var app = angular.module('snippets-manager', []);
+
+  app.run(function($rootScope) {
+
+    /**
+     * Get real file path
+     * @param  {String} path   Relative path, based on `./widget`
+     * @return {String}        Absolute path
+     */
+    $rootScope.url = function (path) {
+      return require.toUrl(path);
+    }
+  })
+  return app;
 });
