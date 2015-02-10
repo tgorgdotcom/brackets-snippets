96ed7ac38cc3a45696452ae024abfffe28ba7b5a
Edward Chu <crazyzyt@gmail.com>
2015-02-10 15:59:10 +0800

    new library ui

--- widget/js/folding.directive.js
+++ widget/js/folding.directive.js
@@ -1,19 +1,21 @@
 define('foldingDirective', ['app'], function(app) {
   app.directive('folding', function($timeout) {
+    var minHeight = 160;
+
     return {
       link: function ($scope, elem) {
 
         $timeout(function() {
-          if (elem.height() > 80) {
+          if (elem.height() > minHeight) {
             elem.data('oriHeight', elem.height());
-            elem.height(80);
+            elem.height(minHeight);
 
             elem
             .on('mouseover', function () {
               elem.height(elem.data('oriHeight'));
             })
             .on('mouseout', function () {
-              elem.height(80);
+              elem.height(minHeight);
             })
           }
         })
